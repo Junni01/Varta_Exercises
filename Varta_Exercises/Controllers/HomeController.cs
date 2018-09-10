f5efe4ac62ef6d665789353dec69b6988eedda33
Junni01 <junnisaku@gmail.com>
2018-09-10 15:25:21 +0300

    Tehtävät #1

--- Varta_Exercises/Controllers/HomeController.cs
+++ Varta_Exercises/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Varta_Exercises.Models;
 
 namespace Varta_Exercises.Controllers
 {
@@ -10,21 +11,37 @@ namespace Varta_Exercises.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+
+
+
+
+            var henkilot = new List<Henkilo>(); // Luodaan listi Henkilöiost johon lisätään dataa.
+
+            henkilot.Add(new Henkilo { ID = 1, ENimi = "Saku", SNImi = "Junni", Pisteet = 20});
+            henkilot.Add(new Henkilo { ID = 2, ENimi = "Pekka", SNImi = "Levänen", Pisteet = 27});
+            henkilot.Add(new Henkilo { ID = 3, ENimi = "Jukka", SNImi = "Raatikainen", Pisteet = 23});
+            henkilot.Add(new Henkilo { ID = 4, ENimi = "Timo", SNImi = "Salmi", Pisteet = 28});
+            henkilot.Add(new Henkilo { ID = 5, ENimi = "Simo", SNImi = "Niemi", Pisteet = 30});
+
+            return View(henkilot); // Lähetetään henkilölista Viewiin. 
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
     }
 }
---
+++ Varta_Exercises/Models/Henkilo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Varta_Exercises.Models
+{
+    public class Henkilo
+    {
+        public int ID { get; set; }
+        public string ENimi { get; set; }
+        public string SNImi { get; set; }
+        public int Pisteet { get; set; }
+
+    }
+}
